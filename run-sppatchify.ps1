2ad0e9fbf550ec66278e1a0fcc76c1318c33fd64
Make Sure IT Happens <MSIH@users.noreply.github.com>
2019-12-02 16:34:01 -0500

    Create run-sppatchify.ps1

---
+++ run-sppatchify.ps1
@@ -0,0 +1,8 @@
+cd C:\DEPL\Scripts
+.\sppatchify.ps1 -downloadMedia
+.\sppatchify.ps1 -showVersionExit
+.\sppatchify.ps1 -testRemotePSExit
+.\sppatchify.ps1 -productlocalExit
+.\sppatchify.ps1 -saveServiceInstanceExit
+.\sppatchify.ps1 -reportContentDatabasesExit
+.\sppatchify.ps1 -ClearCacheIni
